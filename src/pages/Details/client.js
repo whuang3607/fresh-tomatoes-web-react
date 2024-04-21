ae05017a26006f72153c992aa8bbd3f2950baea9
whuang3607 <floridian567@gmail.com>
2024-04-21 12:38:22 -0400

    got the details page to work on the homescreen

--- src/pages/Details/client.js
+++ src/pages/Details/client.js
@@ -12,30 +12,76 @@ const headers = {
   },
 };
 
+const headers2 = {
+  method: "GET",
+  url: "https://imdb146.p.rapidapi.com/v1/title/",
+  params: {id: ""},
+  headers: {
+    'X-RapidAPI-Key': '60b48dd6ddmsh4cdb4dae5c0a232p140d26jsn0159cbb5ffe0',
+    'X-RapidAPI-Host': 'imdb146.p.rapidapi.com'
+  }
+}
+
+let actorsList = []
+
+let genreList = []
+
 export const getMovieById = async (id) => {
   try {
     var movie = '';
     const url = `${MOVIE_API}/${id}`;
     const remoteUrlForTitle = `https://moviesdatabase.p.rapidapi.com/titles/${id}`
-    const remoteUrlForActors = `https://moviesdatabase.p.rapidapi.com/titles/${id}/aka`
-    const remoteUrlForRatings = `https://moviesdatabase.p.rapidapi.com/titles/${id}/ratings`
+    // const remoteUrlForDescription = `https://moviedatabase8.p.rapidapi.com/FindByImbdId/${id}`
+    // headers2.url = remoteUrlForDescription
+    headers2.params.id = `${id}`
+    // const remoteUrlForActors = `https://moviesdatabase.p.rapidapi.com/titles/${id}/aka`
+    // const remoteUrlForRatings = `https://moviesdatabase.p.rapidapi.com/titles/${id}/ratings`
     // console.log(id);
     const response = await axios.get(url);
     if(response.data == null){
-      const [movieResponse, actorsResponse, ratingsResponse] = await Promise.all([
+      const [movieResponse, descriptionResponse] = await Promise.all([
+      // const [movieResponse, actorsResponse, ratingsResponse] = await Promise.all([
         axios.get(remoteUrlForTitle, headers),
-        axios.get(remoteUrlForActors, headers),
-        axios.get(remoteUrlForRatings, headers),
+        axios.request(headers2),
+        // axios.get(remoteUrlForActors, headers),
+        // axios.get(remoteUrlForRatings, headers),
       ]);
       // console.log(movieResponse.data.results)
-      console.log(actorsResponse.data)
+      // console.log(actorsResponse.data)
       // console.log(ratingsResponse.data)
+      // create a section for Genres!!!!
+      // console.log(descriptionResponse.data[0])
+      console.log(descriptionResponse.data)
+
+
+      for (let i = 0; i < descriptionResponse.data.cast.edges.length; i++){
+        if (i < descriptionResponse.data.cast.edges.length - 1){
+          actorsList.push(descriptionResponse.data.cast.edges[i].node.name.nameText.text + ", ");          
+        } else {
+          actorsList.push(descriptionResponse.data.cast.edges[i].node.name.nameText.text);          
+        }
+      }
+      // console.log(actorsList);
+
+      for (let j = 0; j < descriptionResponse.data.genres.genres.length; j++){
+        if (j < descriptionResponse.data.genres.genres.length - 1) {
+          genreList.push(descriptionResponse.data.genres.genres[j].text + ", ");          
+        } else {
+          genreList.push(descriptionResponse.data.genres.genres[j].text);          
+        }
+      }
+      // console.log(genreList);
+
       movie = {
         title: movieResponse.data.results.titleText.text,
-        primary_image: movieResponse.data.results.primaryImage ? movieResponse.data.results.primaryImage : {url: "https://www.dotyeti.com/wp-content/uploads/2023/01/barbie.webp"},
-        actors: actorsResponse.data.results ? actorsResponse.data.results : [],
-        rating: ratingsResponse.data.results ? ratingsResponse.data.results.averageRating : "0",
+        primary_image: movieResponse.data.results.primaryImage.url,
+        release_year: movieResponse.data.results.releaseYear.year,
+        description: descriptionResponse.data.plot?.plotText.plainText || "",
+        actors: actorsList,
+        genres: genreList,
+        // rating: ratingsResponse.data.results ? ratingsResponse.data.results.averageRating : "0",
       }
+      // console.log(movie.primary_image);
     }
     else {
       movie = response.data
